260b1bf81221b62f314f4026deda1009a91aefcd
fedeceliz123 <fede_celiz92@hotmail.com>
2021-08-21 16:03:12 -0300

    primer

---
+++ Stage_Pro/Datos/ConexionDB.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Datos
+{
+    public class ConexionDB
+    {
+        private SqlConnection Conexion = new SqlConnection();
+        private string cadena = "Data Source=FEDE\\SQLEXPRESS;Initial Catalog=stage pro; Integrated Security=True";
+
+
+        public SqlConnection Conetar()
+        {
+
+            try
+            {
+
+                Conexion = new SqlConnection(cadena);
+
+                if (Conexion.State.Equals(ConnectionState.Open))
+                {
+                    Conexion.Close();
+                }
+                else
+                {
+                    Conexion.Open();
+                }
+
+
+            }
+            catch
+            {
+
+
+
+
+
+
+            }
+
+            return Conexion;
+
+        }
+    }
+}
---
+++ Stage_Pro/InterfazU/Paginas/Master/Inicio.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Paginas/Master/Master.Master" AutoEventWireup="true" CodeBehind="Inicio.aspx.cs" Inherits="InterfazU.Paginas.Master.Inicio" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    hola
+</asp:Content>
